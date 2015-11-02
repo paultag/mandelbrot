096f37bf212daf0e6a9f4dceeb2892458fb9ec62
Paul Tagliamonte <tag@pault.ag>
2015-11-01 20:15:04 -0500

    tidy up admin bits

--- mandelbrot/admin.py
+++ mandelbrot/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from mandelbrot.models import Expert, Role, Step, GithubTeam, SlackChannel, OnboardingStep
 
-# Register your models here.
+admin.site.register(Expert)
+admin.site.register(Role)
+admin.site.register(Step)
+
+admin.site.register(GithubTeam)
+admin.site.register(SlackChannel)
+
+admin.site.register(OnboardingStep)
--- mandelbrot/models.py
+++ mandelbrot/models.py
@@ -9,6 +9,9 @@ class Expert(models.Model):
     roles = models.ManyToManyField('Role', related_name="experts")
     steps = models.ManyToManyField('Step', through='OnboardingStep')
 
+    def __str__(self):
+        return "<Expert '{}'>".format(self.name)
+
     def onboard(self):
         if self.onboardings.count() != 0:
             return
@@ -21,17 +24,28 @@ class Expert(models.Model):
 class Role(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self):
+        return "<Role '{}'>".format(self.name)
+
 
 class OnboardingStep(models.Model):
     who = models.ForeignKey('Expert', related_name="onboardings")
     step = models.ForeignKey('Step', related_name="onboardings")
     done = models.BooleanField()
 
+    def __str__(self):
+        return "<OnboardingStep: '{}' '{}' ({})>".format(
+            self.step.description, self.who.name, self.done
+        )
+
 
 class Step(models.Model):
     description = models.CharField(max_length=256)
-    action = models.CharField(max_length=256)  # Python importable name
+    action = models.CharField(max_length=256, blank=True)  # Python importable name
     roles = models.ManyToManyField('Role', related_name="steps")
+
+    def __str__(self):
+        return "<Step: '{}'>".format(self.description)
 
     def get_callable(self):
         module, callable = self.action.rsplit(".", 1)
@@ -47,8 +61,14 @@ class GithubTeam(models.Model):
     team = models.CharField(max_length=128)
     roles = models.ManyToManyField('Role', related_name="githubs")
 
+    def __str__(self):
+        return "<GithubTeam: '{}/{}'>".format(self.org, self.team)
+
 
 class SlackChannel(models.Model):
     team = models.CharField(max_length=128)
     channel = models.CharField(max_length=128)
     roles = models.ManyToManyField('Role', related_name="slacks")
+
+    def __str__(self):
+        return "<SlackChannel: '{}/{}'>".format(self.team, self.channel)
