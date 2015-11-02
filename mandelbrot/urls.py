3992fa48eaaf1d0d75561fd2adf33669b022429e
Paul Tagliamonte <tag@pault.ag>
2015-11-01 20:03:28 -0500

    Un-export some nonsense.

--- mandelbrot/urls.py
+++ mandelbrot/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 
     url(r'^expert/(?P<name>.*)/$', 'mandelbrot.views.expert'),
 
-    url(r'^onboard/(?P<name>.*)/(?P<step>.*)/$', 'mandelbrot.views.process'),
-    url(r'^onboard/(?P<name>.*)/$', 'mandelbrot.views.onboard'),
+    # url(r'^onboard/(?P<name>.*)/(?P<step>.*)/$', 'mandelbrot.views.process'),
+    # url(r'^onboard/(?P<name>.*)/$', 'mandelbrot.views.onboard'),
 ]
