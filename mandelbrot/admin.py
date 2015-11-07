a581b16815db21b4adde26d6104159391c4141e8
Paul Tagliamonte <tag@pault.ag>
2015-11-07 15:40:51 -0500

    foo

--- mandelbrot/admin.py
+++ mandelbrot/admin.py
@@ -40,13 +40,19 @@ class ProjectMembershipInline(admin.TabularInline):
     model = ProjectMember
 
 
+class BadgeAwardInline(admin.TabularInline):
+    model = BadgeAward
+    fk_name = "who"
+
+
 class ContactDetailInline(admin.TabularInline):
     model = ContactDetail
 
 
 class ExpertAdmin(admin.ModelAdmin):
     model = Expert
-    inlines = [ProjectMembershipInline, ContactDetailInline, OnboardingStepInline]
+    inlines = [ProjectMembershipInline, ContactDetailInline,
+               BadgeAwardInline, OnboardingStepInline]
 
 
 admin.site.register(Expert, ExpertAdmin)
