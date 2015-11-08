3946f9a0a286773cce0a5a37fecf11c86760f86d
Paul Tagliamonte <tag@pault.ag>
2015-11-08 08:53:31 -0500

    add in login html

--- public/settings.py
+++ public/settings.py
@@ -5,6 +5,9 @@ SECRET_KEY = 'changeme'
 DEBUG = True
 ALLOWED_HOSTS = []
 
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL  = '/'
+
 # Application definition
 
 INSTALLED_APPS = (
@@ -49,10 +52,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'public.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -61,22 +60,11 @@ DATABASES = {
 }
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
 
--- public/urls.py
+++ public/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
+
 import mandelbrot.urls
 
 urlpatterns = [
+
+    url(regex=r'^login/$', view=login, kwargs={'template_name': 'mandelbrot/login.html'}, name='login'),
+    url(regex=r'^logout/$', view=logout, kwargs={'next_page': '/'}, name='logout'),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(mandelbrot.urls)),
 ]
