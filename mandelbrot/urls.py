d7918b14ba3b2ee52124c231623e7f9328bcdee9
Paul Tagliamonte <tag@pault.ag>
2016-01-30 16:10:48 -0500

    clean up urls

--- mandelbrot/urls.py
+++ mandelbrot/urls.py
@@ -1,16 +1,15 @@
 from django.conf.urls import url
+import mandelbrot.views
 
 urlpatterns = [
-    url(r'^$', 'mandelbrot.views.home'),
+    url(r'^$', mandelbrot.views.home),
 
-    url(r'^experts/$', 'mandelbrot.views.experts', name='experts'),
-    url(r'^expert/(?P<name>.*)/$', 'mandelbrot.views.expert', name='expert'),
+    url(r'^experts/$', mandelbrot.views.experts, name='experts'),
+    url(r'^expert/(?P<name>.*)/$', mandelbrot.views.expert, name='expert'),
 
-    url(r'^projects/$', 'mandelbrot.views.projects', name='projects'),
-    url(r'^project/(?P<id>.*)/$', 'mandelbrot.views.project', name='project'),
+    url(r'^projects/$', mandelbrot.views.projects, name='projects'),
+    url(r'^project/(?P<id>.*)/$', mandelbrot.views.project, name='project'),
 
-    url(r'^offices/$', 'mandelbrot.views.offices', name='offices'),
-    url(r'^office/(?P<id>.*)/$', 'mandelbrot.views.office', name='office'),
-
-    url(r'^welcome/(?P<expert>.*)/$', 'mandelbrot.views.welcome', name='welcome'),
+    url(r'^offices/$', mandelbrot.views.offices, name='offices'),
+    url(r'^office/(?P<id>.*)/$', mandelbrot.views.office, name='office'),
 ]
